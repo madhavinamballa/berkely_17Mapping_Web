2d1ea6477f9d66fe398ae914f558b0ede151a25b
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2020-09-09 18:12:32 -0700

    added part2 part3

---
+++ 17-Mapping-Web_part2/17-Mapping-Web_part2/2/Activities/01-Evr_BasicNYCBoroughs/Solved/static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+//const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY ="pk.eyJ1IjoibWFkaGF2aW4iLCJhIjoiY2szcW8wZWNoMDNwMjNsbXFkbjM4bWQ0cSJ9.DGKgiHwWGjq70trFNG-weQ"
---
+++ 17-Mapping-Web_part2/17-Mapping-Web_part2/2/Activities/03-Stu_MarkerClusters/Unsolved/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWFkaGF2aW4iLCJhIjoiY2szcW8wZWNoMDNwMjNsbXFkbjM4bWQ0cSJ9.DGKgiHwWGjq70trFNG-weQ";
